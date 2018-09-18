e50e887e4e4cd115d54e026bebdca0b483ba389e
Olagunju Abdul-Hammid Ede <akaniolagunju@yahoo.com>
2018-09-18 17:24:23 -0600

    database and feed update

--- main/main.go
+++ main/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"html/template"
 	"io"
@@ -8,7 +9,8 @@ import (
 	"net/http"
 	"os"
 	"strconv"
-	"time"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 //Post is good
@@ -16,6 +18,17 @@ type Post struct {
 	Title       string
 	Description string
 	Time        string
+}
+
+//Project object represent each project
+type Project struct {
+	Title       string
+	Description string
+	Duration    float64
+	Cost        float64
+	Sector      string
+	Time        []uint8
+	Image       string
 }
 
 func main() {
@@ -30,17 +43,49 @@ func main() {
 	}
 }
 
+//Handels requests to the hompage
+
 func myHomePage(w http.ResponseWriter, req *http.Request) {
+
+	db, err := sql.Open("mysql", "root:atinuke22@tcp(127.0.0.1:3306)/test")
+	checkerr(err)
+	defer db.Close()
+	rows, err := db.Query("SELECT * FROM project ORDER BY time DESC")
+	checkerr(err)
+
 	t, _ := template.ParseFiles("src/sca1/templates/home.html", "src/sca1/templates/header.html", "src/sca1/templates/footer.html")
-	Description := "Etiam mollis lectus et laoreet faucibus. Suspendisse facilisis tellus velit, varius scelerisque leo sollicitudin at. Aliquam erat volutpat. Sed et varius quam. Cras aliquam odio a odio ornare, ut dapibus tortor cursus. Vestibulum eget erat libero. Duis ornare ligula ac elit gravida, at hendrerit dolor cursus. Proin sit amet magna eros. Proin posuere nibh a consectetur aliquam. Aenean non ligula faucibus urna finibus blandit a ut ligula."
-	Title := "Trans Mountain's delay costs Canadians $40 million every day."
-	myPost := Post{Title, Description, "2 days"}
+	// Description := "Etiam mollis lectus et laoreet faucibus. Suspendisse facilisis tellus velit, varius scelerisque leo sollicitudin at. Aliquam erat volutpat. Sed et varius quam. Cras aliquam odio a odio ornare, ut dapibus tortor cursus. Vestibulum eget erat libero. Duis ornare ligula ac elit gravida, at hendrerit dolor cursus. Proin sit amet magna eros. Proin posuere nibh a consectetur aliquam. Aenean non ligula faucibus urna finibus blandit a ut ligula."
+	// Title := "Trans Mountain's delay costs Canadians $40 million every day."
+
 	t.ExecuteTemplate(w, "header.html", nil)
-	t.ExecuteTemplate(w, "home.html", myPost)
+	for rows.Next() {
+		var id int
+		var uid int
+		var title string
+		var description string
+		var cost float64
+		var duration float64
+		var sector string
+		var time []uint8
+
+		err := rows.Scan(&id, &uid, &title, &description, &cost, &duration, &time, &sector)
+		checkerr(err)
+
+		//Get the image from project_image table
+		stmt, err := db.Prepare("SELECT image_name FROM project_media WHERE project_id =?")
+		checkerr(err)
+		var imageName string
+		err = stmt.QueryRow(id).Scan(&imageName)
+		// fmt.Println(imageName)
+		myProject := Project{title, description, duration, cost, sector, time, imageName}
+		t.ExecuteTemplate(w, "home.html", myProject)
+	}
+
 	t.ExecuteTemplate(w, "footer.html", nil)
 
 }
 
+//Handels requests to the signup page
 func signup(w http.ResponseWriter, req *http.Request) {
 	t, _ := template.ParseFiles("src/sca1/templates/signup.html", "src/sca1/templates/header.html", "src/sca1/templates/footer.html")
 	t.ExecuteTemplate(w, "header.html", nil)
@@ -49,6 +94,7 @@ func signup(w http.ResponseWriter, req *http.Request) {
 
 }
 
+//Handels requests to the login page
 func login(w http.ResponseWriter, req *http.Request) {
 	t, _ := template.ParseFiles("src/sca1/templates/login.html", "src/sca1/templates/header.html", "src/sca1/templates/footer.html")
 	t.ExecuteTemplate(w, "header.html", nil)
@@ -57,16 +103,7 @@ func login(w http.ResponseWriter, req *http.Request) {
 
 }
 
-//Project object represent each project
-type Project struct {
-	Title       string
-	Description string
-	Duration    float64
-	Cost        float64
-	Sector      string
-	Time        int64
-	Image       string
-}
+//Handels requests for adding a new project
 
 func newProject(w http.ResponseWriter, req *http.Request) {
 	if req.Method == "POST" {
@@ -83,7 +120,7 @@ func newProject(w http.ResponseWriter, req *http.Request) {
 
 		defer file.Close()
 		image := handler.Filename
-		f, err := os.OpenFile("src/sca1/public/img/test/"+handler.Filename, os.O_WRONLY|os.O_CREATE, 0666)
+		f, err := os.OpenFile("src/sca1/public/img/test/"+image, os.O_WRONLY|os.O_CREATE, 0666)
 		if err != nil {
 			fmt.Println(err)
 			return
@@ -95,30 +132,48 @@ func newProject(w http.ResponseWriter, req *http.Request) {
 		duration, _ := strconv.ParseFloat(req.FormValue("duration"), 64)
 		cost, _ := strconv.ParseFloat(req.FormValue("cost"), 64)
 		sector := req.FormValue("sector")
-		time := time.Now().Unix()
+		// time := make([]uint8, 15)
 
-		t, err := template.ParseFiles("src/sca1/templates/temp.html", "src/sca1/templates/header.html", "src/sca1/templates/footer.html")
-		// if err != nil {
-		t.ExecuteTemplate(w, "header.html", nil)
-		t.ExecuteTemplate(w, "temp.html", Project{title, description, duration, cost, sector, time, image})
-		t.ExecuteTemplate(w, "footer.html", nil)
-
-		fmt.Println(title)
-		fmt.Println(description)
-		fmt.Println(duration)
-		fmt.Println(cost)
-		fmt.Println(sector)
-		fmt.Println(time)
 		// }
+
+		db, err := sql.Open("mysql", "root:atinuke22@tcp(127.0.0.1:3306)/test")
+
+		//Prepare stmt top avoid sql injection
+		stmt, err := db.Prepare("INSERT project SET user_id=?, title=?, description=?, cost=?, duration=?, sector=?")
+		checkerr(err)
+
+		//execute the statement abd store the returned value in res
+		res, err := stmt.Exec(1, title, description, cost, duration, sector)
+		checkerr(err)
+
+		//
+		id, err := res.LastInsertId()
+
+		stmtImg, err := db.Prepare("INSERT project_media SET project_id=?, image_name=?")
+		checkerr(err)
+
+		resImg, err := stmtImg.Exec(id, image)
+		checkerr(err)
+		fmt.Println(resImg.LastInsertId())
+
+		// t, err := template.ParseFiles("src/sca1/templates/temp.html", "src/sca1/templates/header.html", "src/sca1/templates/footer.html")
+		// // if err != nil {
+		// t.ExecuteTemplate(w, "header.html", nil)
+		// t.ExecuteTemplate(w, "temp.html", Project{title, description, duration, cost, sector, time, image})
+		// t.ExecuteTemplate(w, "footer.html", nil)
+
+		myHomePage(w, req)
+
+		// fmt.Println(title)
+		// fmt.Println(description)
+		// fmt.Println(duration)
+		// fmt.Println(cost)
+		// fmt.Println(sector)
+		// fmt.Println(time)
 
 	} else {
 		fmt.Println("Method is get...")
 	}
-}
-
-func myTemplate(templateName string) (temp *template.Template, err error) {
-	t, err := template.ParseFiles("src/sca1/templates/"+templateName+".html", "src/sca1/templates/header.html", "src/sca1/templates/footer.html")
-	return t, err
 }
 
 // func showTime(sec int64) (time string) {
@@ -132,3 +187,9 @@ func myTemplate(templateName string) (temp *template.Template, err error) {
 // 		return strconv.FormatInt(int64(math.Floor(float64(sec/60*60))), 10) + " hours ago"
 // 	}
 // }
+
+func checkerr(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
